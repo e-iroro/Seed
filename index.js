6cb9da297b0452221587c47dddcf6d8353c6e6ed
e-iroro <92281014+e-iroro@users.noreply.github.com>
2021-10-30 16:00:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+/*console.log("Hello software development");
+
+var firstName = "Emmanuella";
+
+console.log(firstName);
+
+var middleName = "Eruoghene";
+
+const PI = 3.14;
+
+const ear = 453
+
+console.log(middleName);
+
+var lastName = "Iroro"
+
+console.log(lastName);
+
+console.log(PI);
+
+console.log(ear);
+
+console.log(48+45); 
+
+console.log(41-8);
+
+console.log(84/22);
+
+console.log(63*22);
+
+console.log(623**20);
+
+console.log(75%3);
+
+
+let string1 = 'happy';
+
+let string2 = 'birthday';
+
+let string3 = 'Michael';
+
+let newString = string1 + string3 + string2;
+
+console.log(newString);
+*/
+
+let months = ['January', 'February', 'March', 'April', 'May'];
+i=0
+while (i < months.length) {
+    console.log(months[i])
+    i++;
+}
+
+
+
+console.log(months);
+
+var mango;
+mango=5;
+var coco;
+coco=mango;
+console.log(coco);
+
+let bloodGroup = "O+"
+
+switch(bloodGroup) {
+  case "A":
+    console.log("My blood group is A");
+    break;
+  case "B":
+    console.log ("My blood group is B");
+    break;
+  case "AB":
+    console.log ("My blood group is AB");
+    break;
+  case "O+":
+    console.log ("My blood group is O+");
+    break;
+  default:
+    console.log("My blood group is not listed here");
+}
+
+function checkAge(num){
+    if (num < 13){
+        console.log("you are not a teen");
+    }else
+        console.log("you are above 13");
+    }
+
+    checkAge(33);
+
+
