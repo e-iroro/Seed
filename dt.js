1659f1e2a8da66337bb13be7e95e2606fadcc4b8
e-iroro <92281014+e-iroro@users.noreply.github.com>
2021-11-04 19:18:56 +0100

    Add files via upload

---
+++ dt.js
@@ -0,0 +1,28 @@
+let x=7;
+console.log(typeof x);
+
+let y=true; ///true or false
+console.log(typeof y);
+
+let z= 'hello world'; ///anything inside quotes
+console.log(typeof z);
+
+
+let a;
+console.log(a);
+console.log(typeof a);
+
+
+let months = ['January', 'February', 'March', 'April', 'May'];
+i=0
+while (i < 3) {
+    console.log(months[i])
+    i++;
+}
+let months = ['January', 'February', 'March', 'April', 'May'];
+reversedMonths = [];
+for ( i = 0; i < months.length; i++) {
+    reversedMonths.unshift(months[i]);
+}
+console.log(reversedMonths);
+
